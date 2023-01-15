aafe6c1b79c60250f7fac8770c9d8d8293ec948a
Nikhil Kathait <nknick97@gmail.com>
2023-01-15 15:37:43 +0530

    Add project files.

---
+++ refReturnExample/Program.cs
@@ -0,0 +1,42 @@
+﻿class Student
+{
+    //Public field
+    public int grade = 2;
+
+    //Public method
+    public void PrintGrade()
+    {
+        System.Console.WriteLine("Grade : " + grade);
+    }
+
+    //Public method with ref return
+    public ref int DoWork()
+    {
+        return ref grade;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        //Creating object of student
+        Student s = new Student();
+
+        s.PrintGrade(); //Output Grade : 2
+
+        //Call DoWork
+        ref int g = ref s.DoWork();
+
+        //Call PrintGrade
+        s.PrintGrade(); //Output Grade : 2
+
+        //Update the value of 'ref return'
+        g = 5;
+
+        //Call PrintGrade after updating the value of 'ref return'
+        s.PrintGrade(); //Output Grade : 5
+
+        System.Console.ReadKey();
+    }
+}
